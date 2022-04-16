ef903e0783fa3e147a9c274a31dfd8fb491151d7
Victor Prado <61568003+victorpraado1005@users.noreply.github.com>
2022-04-16 01:46:55 -0300

    Add files via upload
    Alteração da API para uma gratuita e sem limite de requisição.
    
    Ajuste na função do JS para receber o valor inserido pelo usuário.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function converterMoeda(moeda, amount){
-    let url = "https://v6.exchangerate-api.com/v6/45b58dbce00ed6d35c4e0d49/pair/"+ moeda  + "/BRL/" + amount;
-    console.log(url)
+function converterMoeda(){
+    let moeda = document.getElementById("moeda").value
+    let totalConversao = document.getElementById("total").value
+
+    let url = "https://economia.awesomeapi.com.br/"+ moeda;
 
     fetch(url).then((res)=>{
         return res.json();
     }).then((data)=>{
-        let rate = data.conversion_rate
-        let total = rate * amount;
-        document.getElementById("moeda").innerHTML = rate;
+        let rate = data[0].bid
+        let total = rate * totalConversao;
+        document.getElementById("currency").innerHTML = rate;
         document.getElementById("conversao").innerHTML = total.toFixed(2);
     })
 }
-
-converterMoeda("EUR", 10)
