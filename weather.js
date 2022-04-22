89526094b00c449758e4347db6f48a450ee6a846
Victor Prado <61568003+victorpraado1005@users.noreply.github.com>
2022-04-22 02:55:21 -0300

    Add files via upload
    Alteração de API para outro endpoint para obter as informações necessárias. API funcionando 100%.

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
-let API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=pt_br&q="
+let API_URL = "http://api.weatherapi.com/v1/forecast.json?key="
 
-let API_KEY = "6bc765672523aa609df67f28de838da5"
+let API_KEY = "177f54f66a33404088153326222204"
 
 function weatherApi(){
     //variavel para puxar a informação do nome da cidade inserida pelo usuário
@@ -8,16 +8,15 @@ function weatherApi(){
     //chamada da função de tratamento da string para deixar conforme o aceite da url
     string = tratamentoString(string)
     //junção das variaveis para montar a url final que será responsável por puxar os dados
-    let finalURL = API_URL + string + "&appid=" + API_KEY;
+    let finalURL = API_URL + API_KEY + "&q=" + string + "&days=1&aqi=no&alerts=no";
 
     fetch(finalURL).then((res)=>{
         return res.json()
     }).then((data)=>{
-        console.log(data)
-        let weather_city_name = data.name
-        let weather_temp = data.main.temp
-        let weather_temp_min = data.main.temp_min
-        let weather_temp_max = data.main.temp_max
+        let weather_city_name = data.location.name
+        let weather_temp = data.current.temp_c
+        let weather_temp_min = data.forecast.forecastday[0].day.mintemp_c
+        let weather_temp_max = data.forecast.forecastday[0].day.maxtemp_c
         document.getElementById("weather_city_name").innerHTML = weather_city_name
         document.getElementById("weather_temp").innerHTML = weather_temp
         document.getElementById("weather_temp_min").innerHTML = weather_temp_min
@@ -27,9 +26,7 @@ function weatherApi(){
 }
 
 function tratamentoString(string){
-    string = string.replaceAll(' ', '+')
+    string = string.replaceAll(' ', '%20')
     string = string.normalize("NFD").replace(/[\u0300-\u036f]/g, '')
     return string
 }
-
-//https://api.openweathermap.org/data/2.5/weather?q=Sao+Jose+do+Rio+Preto&lang=pt-br&appid=6bc765672523aa609df67f28de838da5&units=metric
