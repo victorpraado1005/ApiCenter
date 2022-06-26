3692df69c394e2f74df8af72218eb1f541a916b6
Victor Prado <61568003+victorpraado1005@users.noreply.github.com>
2022-06-26 02:01:25 -0300

    adicionando jquery para as imagens de background

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $("body").addClass('api_weather_background');
+    $("#weatherAPI").show()
+})
+
+$("#btnWeatherAPI").click(()=>{            
+    $("#weatherAPI").show()
+    $("#exchangeRatesAPI").hide()
+    $("#cepAPI").hide()
+    $("body").removeClass('api_money_background');
+    $("body").removeClass('api_viacep_background');
+    $("body").addClass('api_weather_background');
+})
+
+$("#btnExchangeRatesAPI").click(()=>{
+    $("#exchangeRatesAPI").show()
+    $("#weatherAPI").hide()
+    $("#cepAPI").hide()
+    $("body").removeClass('api_weather_background');
+    $("body").removeClass('api_viacep_background');
+    $("body").addClass('api_money_background');
+})
+
+$("#btnViaCepAPI").click(()=>{
+    $("#cepAPI").show()
+    $("#exchangeRatesAPI").hide()
+    $("#weatherAPI").hide()
+    $("body").removeClass('api_weather_background');
+    $("body").removeClass('api_money_background');
+    $("body").addClass('api_viacep_background');
+})
